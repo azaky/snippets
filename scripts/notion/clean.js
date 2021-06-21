f846102b913c1e886749690c2e6330aeaf2b441b
Ahmad Zaky <ahmadzaky003@gmail.com>
2021-06-22 05:14:04 +0700

    beautify + use UTC +  add last_sync in posts.json

--- scripts/notion/clean.js
+++ scripts/notion/clean.js
@@ -6,23 +6,23 @@ const root = path.join(__dirname, "../..");
 try {
   fs.rmSync(path.join(root, "_posts"), { recursive: true, force: true });
 } catch (e) {
-  console.warn(e);
+  console.warn(e.message);
 }
 
 try {
   fs.rmSync(path.join(root, "_tags"), { recursive: true, force: true });
 } catch (e) {
-  console.warn(e);
+  console.warn(e.message);
 }
 
 try {
   fs.rmSync(path.join(root, "posts.json"));
 } catch (e) {
-  console.warn(e);
+  console.warn(e.message);
 }
 
 try {
   fs.rmSync(path.join(root, "COMMIT_MESSAGE"));
 } catch (e) {
-  console.warn(e);
+  console.warn(e.message);
 }
--- scripts/notion/sync.js
+++ scripts/notion/sync.js
@@ -3,14 +3,13 @@ const path = require("path");
 const util = require("util");
 const dateformat = require("dateformat");
 const yaml = require("js-yaml");
-
 const { Client } = require("@notionhq/client");
-
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
 const { NotionAPI } = require("notion-client");
 const { NotionRenderer } = require("react-notion-x");
 const { JSDOM } = require("jsdom");
+const { html_beautify } = require("js-beautify");
 
 const Code = (props) => {
   const { code, language } = props;
@@ -45,24 +44,38 @@ const dumpStringToFile = (str, filename) =>
   fs.writeFileSync(filename, str, "utf8");
 
 module.exports = async () => {
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  });
-
   const postsRoot = path.join(root, "_posts");
   if (!fs.existsSync(postsRoot)) {
     fs.mkdirSync(postsRoot);
   }
 
-  console.log(`Found ${response.results.length} posts from Notion`);
-
   let previousPosts = [];
+  let lastSync = new Date(0);
   if (fs.existsSync(postsFilename)) {
-    previousPosts = JSON.parse(fs.readFileSync(postsFilename, "utf8"));
+    const prevData = JSON.parse(fs.readFileSync(postsFilename, "utf8"));
+    previousPosts = prevData.posts;
+    lastSync = new Date(prevData.lastSync);
   }
+
+  const now = new Date();
+
+  let updated = false;
   const posts = [];
-  const allTags = [];
+  const uniqueTags = new Set();
   const actions = [];
+
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    // When filtered by last edited, we can't possibly know if a post is deleted or not.
+    // filter: {
+    //   property: "Last edited time",
+    //   date: {
+    //     after: dateformat(lastSync, "isoUtcDateTime"),
+    //   },
+    // },
+  });
+
+  console.log(`Found ${response.results.length} posts from Notion`);
 
   for (const page of response.results) {
     const { id, last_edited_time, created_time } = page;
@@ -71,11 +84,11 @@ module.exports = async () => {
     const tags = (page.properties.Tags?.multi_select ?? []).map(
       (tag) => tag.name
     );
-    const date = page.properties.Date?.date?.start ?? created_time;
+    const date = new Date(page.properties.Date?.date?.start ?? created_time);
 
     console.log(`Processing post id = ${id}, title = ${title}`);
 
-    allTags.push(...tags);
+    tags.forEach(tag => uniqueTags.add(tag));
 
     const previousData = previousPosts.find((data) => data.id === id);
 
@@ -112,7 +125,7 @@ module.exports = async () => {
     }
 
     const recordMap = await notionXClient.getPage(id);
-    const html = ReactDOMServer.renderToStaticMarkup(
+    let html = ReactDOMServer.renderToStaticMarkup(
       <NotionRenderer
         recordMap={recordMap}
         components={{
@@ -124,6 +137,12 @@ module.exports = async () => {
       />
     );
 
+    // Remove class="notion-block-uuid" everywhere.
+    html = html.replace(
+      / ?notion-block-[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/g,
+      ""
+    );
+
     // Get post excerpt from first notion-text block, if any.
     const dom = new JSDOM(html);
     const firstParagraph =
@@ -133,7 +152,7 @@ module.exports = async () => {
     const frontMatter = {
       layout: "post",
       title,
-      date: dateformat(date, "yyyy-mm-dd HH:MM:ss p"),
+      date: dateformat(date, "yyyy-mm-dd HH:MM:ss p", true),
       tags,
       excerpt,
       notion_id: id.replace(/\-/g, ""),
@@ -144,7 +163,11 @@ module.exports = async () => {
       .filter((t) => !!t)
       .map((t) => t.toLowerCase())
       .join("-");
-    const filename = `${dateformat(date, "yyyy-mm-dd")}-${titleParamCase}.html`;
+    const filename = `${dateformat(
+      date,
+      "yyyy-mm-dd",
+      true
+    )}-${titleParamCase}.html`;
 
     // Erase previous file first, in case filename changes.
     if (previousData?.filename) {
@@ -155,9 +178,20 @@ module.exports = async () => {
       }
     }
 
+    const formattedFrontMatter = [
+      "---",
+      yaml.dump(frontMatter, { lineWidth: 80 }).replace(/\\n$/, ""),
+      "---",
+    ].join("\n");
+
+    const formattedHtml = html_beautify(html, {
+      indent_size: 2,
+      wrap_line_length: 80,
+    });
+
     fs.writeFileSync(
       path.join(postsRoot, filename),
-      ["---", yaml.dump(frontMatter), "---", "", html].join("\n"),
+      formattedFrontMatter + "\n\n" + formattedHtml,
       "utf8"
     );
 
@@ -165,9 +199,11 @@ module.exports = async () => {
       id,
       title,
       filename,
+      date,
       last_edited_time,
     });
 
+    updated = true;
     if (previousData) {
       actions.push(`Update "${title}"`);
     } else {
@@ -177,10 +213,14 @@ module.exports = async () => {
     console.log(`Processing post id = ${id}, title = ${title} done!`);
   }
 
+  if (!updated) {
+    console.log("No changes found");
+    return;
+  }
+
   console.log("Processing tags ...");
 
-  const uniqueTags = [...new Set(allTags)];
-  console.log(`Found ${uniqueTags.length} tags`);
+  console.log(`Found ${uniqueTags.size} tags`);
 
   const tagsRoot = path.join(root, "_tags");
   if (fs.existsSync(tagsRoot)) {
@@ -197,7 +237,12 @@ module.exports = async () => {
   console.log("Processing tags done!");
 
   console.log("Exporting posts.json ...");
-  fs.writeFileSync(postsFilename, JSON.stringify(posts), "utf8");
+  posts.sort((a, b) => a.date < b.date ? -1 : 1);
+  fs.writeFileSync(
+    postsFilename,
+    JSON.stringify({ last_sync: now, posts }, null, 2),
+    "utf8"
+  );
   console.log("Exporting posts.json done!");
 
   // A workaround to set commit message.
@@ -205,7 +250,10 @@ module.exports = async () => {
   if (actions.length === 1) {
     message = `sync_notion: ${actions[0]}`;
   } else if (actions.length > 1) {
-    message = [`sync_notion: multiple actions`, ...actions.map(s => `- ${s}`)].join("\n");
+    message = [
+      `sync_notion: multiple actions`,
+      ...actions.map((s) => `- ${s}`),
+    ].join("\n");
   }
   fs.writeFileSync(path.join(root, "COMMIT_MESSAGE"), message + "\n", "utf8");
 };
